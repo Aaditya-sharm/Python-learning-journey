590b276cb62340c79423bb0c5595857279849352
Aaditya <snp.aadi@gmail.com>
2026-03-25 11:50:54 +0530

    Added Day 2 string practice programs

--- Day-2/string_practice.py
+++ Day-2/string_practice.py
@@ -1,1 +1,22 @@
 
+#Length + First + Last Character
+name = input("Enter your name: ")
+if len(name) > 0:
+    print("Length:", len(name))
+    print("First character:", name[0])
+    print("Last character:", name[-1])
+else:
+    print("Empty string entered")
+
+#Reverse String
+name= input("Enter your name: ")
+print("Reversed:", name[::-1])
+
+#Concatenation
+str1 = "Hello"
+str2 = "World"
+
+result = str1 + " " + str2
+print(result)
+
+#
